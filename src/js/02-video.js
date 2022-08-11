0f4e3e5476058dc2619a53d510928e75107e8151
MaksimGordienko89 <99180492+MaksimGordienko89@users.noreply.github.com>
2022-08-11 12:11:21 +0300

    edit task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,9 +1,20 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+var throttle = require('lodash.throttle');
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+let savedTime;
 
-player.on('timeupdate', onPlay);
+player.on('timeupdate', throttle(onPlay, 1000));
+
 function onPlay(data) {
   localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+  console.log(localStorage.getItem('videoplayer-current-time'));
+}
+
+savedTime = localStorage.getItem('videoplayer-current-time');
+
+if (savedTime) {
+  player.setCurrentTime(JSON.parse(savedTime).seconds);
 }
