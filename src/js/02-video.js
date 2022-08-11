c663307c717f5698b35cc17aab8e0b9c09b02ea5
MaksimGordienko89 <99180492+MaksimGordienko89@users.noreply.github.com>
2022-08-11 11:56:25 +0300

    task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,9 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', onPlay);
+function onPlay(data) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+}
